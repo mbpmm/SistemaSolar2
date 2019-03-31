a44142bbe3f715ca1d3b9c29a74a4472079a6134
mbpmm <marianomcr92@gmail.com>
2019-03-31 19:15:59 -0300

    Paso el proyecto a otro repositorio, funciona el gitignore

---
+++ SistemaSolar/Assets/codigo/Camara.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    public Transform[] target;
+    float changeTarget;
+    public float timeLimit;
+    Vector3 initialPos;
+    public Vector3 offset;
+    int aux;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPos = transform.position;
+        changeTarget = 0;
+        aux = 0;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        changeTarget += Time.deltaTime;
+        transform.position = target[aux].position + offset;
+        if (changeTarget > timeLimit)
+        {
+            changeTarget = 0;
+            aux++;
+        }
+        if (aux==target.Length)
+        {
+            aux = 0;
+        }
+    }
+}
---
+++ SistemaSolar/Assets/codigo/Planeta.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planeta : MonoBehaviour
+{
+    public string nombre;
+    public float speed;
+    public float radio;
+    public float angulo;
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        angulo = 0;
+        radio = transform.position.y;
+        Debug.Log("Nombre: " + nombre);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angulo += speed*Time.deltaTime;
+        transform.position =new Vector3 ( 0, radio * Mathf.Cos(angulo), radio * Mathf.Sin(angulo) );
+        transform.Rotate(new Vector3(rotationSpeed , 0, 0),rotationSpeed,Space.Self);
+    }
+}
